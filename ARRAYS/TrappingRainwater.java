e14fe1a5962285e13cfdbdadb38d3fd04f6fbe2e
SOORAJ SUBRAMANIAN <120247011+I-Am-Sooraj@users.noreply.github.com>
2025-09-10 13:56:48 +0530

    Trapping RainWater Modifies

--- ARRAYS/TrappingRainwater.java
+++ ARRAYS/TrappingRainwater.java
@@ -1,1 +1,16 @@
-
+import java.lang.reflect.Array;
+import java.util.*;
+public class TrappingRainwater{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter The Size of Array:");
+        int size = sc.nextInt();
+        int[] arr = new int[size];
+        System.out.println("Enter The Elements of Array");
+        for(int i=0;i<size;i++){
+            System.out.print("Value at index "+i+" is equal to: "); 
+            arr[i] = sc.nextInt();
+        }
+        System.out.println("Following Array Has been Created:" + Arrays.toString(arr));
+}    
+}
